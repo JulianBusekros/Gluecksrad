e1ba4c5888146da3478ef0fbf15f50df29ada392
Julian <julian.busekros@quinscape.de>
2021-11-05 10:07:27 +0100

    isValidUserInput() und Methode für Output hinzugefügt
    -die Methode isValidUserInput() überprüft, ob der User nur einen Buchstaben und keine Zahlen/Sonderzeichen eingibt
    -die Methode outputAttemptsAndWordProgress() gibt den derzeitigen Versuch und den Fortschritt des zu erratenden Wordes aus
    -Änderungen bei der Benennung von nicht eindeutigen Variablen

---
+++ src/de/quinscape/GameTest.java
@@ -0,0 +1,31 @@
+package de.quinscape;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.channels.AsynchronousServerSocketChannel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void outputAttemptsAndWordProgress() {
+    }
+
+    @Test
+    void isValidUserInput() {
+    }
+
+    @Test
+    void isGameOver() {
+    }
+
+    @Test
+    void getRandomWord() {
+    }
+
+    @Test
+    void generateWordWithUnderscores() {
+    }
+}
