73c5be286e6469a3809d414fbd42f227e26c312b
JulianQS <88379505+JulianBusekros@users.noreply.github.com>
2021-11-05 09:45:04 +0100

    Game Development (#1)
    Erstes lauffähiges Spiel.

---
+++ src/de/quinscape/Game.java
@@ -0,0 +1,96 @@
+package de.quinscape;
+
+import java.util.*;
+
+public class Game {
+
+    private final Random random = new Random();
+    private final Scanner scanner = new Scanner(System.in);
+    private int currentAttempt;
+    private int numberOfGuessedChars;
+    List<String> words;
+
+    public Game() {
+        this.words = Arrays.asList(
+                "Quinscape",
+                "Antarktis",
+                "Halloween",
+                "Ahornblatt",
+                "Hochhaus"
+        );
+    }
+
+    public void startGame(){
+        System.out.println("===Willkommen beim Glücksrad===");
+        String wordToGuess = getRandomWord(words);
+        String underscoreWord = wordToGuess.replaceAll("[A-Za-z]", "_");
+        System.out.println(underscoreWord);
+
+        boolean runningGame = true;
+
+        while(runningGame){
+            
+            String guessedChar = scanner.nextLine();
+
+            if (!isValidUserInput(guessedChar)){
+                outputAttemptsAndWordProgress(underscoreWord);
+                continue;
+            }
+            currentAttempt++;
+            underscoreWord = generateWordWithUnderscores(wordToGuess, guessedChar, underscoreWord);
+            outputAttemptsAndWordProgress(underscoreWord);
+
+            runningGame = isGameOver(wordToGuess, underscoreWord);
+
+        }
+    }
+    public void outputAttemptsAndWordProgress(String underscoreWord){
+        System.out.println(underscoreWord);
+        System.out.println("Versuch: " + currentAttempt );
+    }
+    public boolean isValidUserInput(String guessedChar){
+        if(guessedChar.length() != 1 || !guessedChar.matches("[A-Za-z]")){
+            System.out.println("Bitte nur einen Buchstaben eingeben!");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isGameOver(String wordToGuess, String underscoreWord){
+        if(currentAttempt >= wordToGuess.length()*2){
+            System.out.printf("In %s Versuchen haben sie %s richtige Buchstaben erraten. Das richtige Wort ist %s%n"
+                    , currentAttempt, numberOfGuessedChars, wordToGuess);
+            return false;
+        } else if(underscoreWord.equals(wordToGuess)){
+            System.out.printf("In %s Versuchen haben sie das Wort %s erraten.", currentAttempt, wordToGuess);
+            return false;
+        }
+        return true;
+    }
+
+    public String getRandomWord(List<String> words){
+        int randomIndex = random.nextInt(words.size());
+        return words.get(randomIndex);
+    }
+
+    public String generateWordWithUnderscores(String wordToGuess, String guessedCharacter, String underscoreWord){
+        StringBuilder wordWithUnderscores = new StringBuilder(underscoreWord);
+        char guessedChar = guessedCharacter.charAt(0);
+
+        for (int i = 0; i < wordToGuess.length(); i++){
+            if(wordToGuess.toLowerCase().charAt(i) == guessedChar){
+                if(i == 0){
+                    wordWithUnderscores.setCharAt(i, Character.toUpperCase(guessedChar));
+                } else {
+                    wordWithUnderscores.setCharAt(i, guessedChar);
+
+                }
+                numberOfGuessedChars++;
+            }
+        }
+        return wordWithUnderscores.toString();
+    }
+
+
+
+}
--- src/de/quinscape/Main.java
+++ src/de/quinscape/Main.java
@@ -3,6 +3,8 @@ package de.quinscape;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Game game = new Game();
+        game.startGame();
     }
 }
